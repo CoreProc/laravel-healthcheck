c724eec09e6effcabae98ee0d498ac56cb799ee7
Chris Bautista <chris.bautista@coreproc.com>
2024-09-17 12:43:12 +0800

    Handle missing pushedAt variables

--- src/Listeners/LogJobProcessedListener.php
+++ src/Listeners/LogJobProcessedListener.php
@@ -14,7 +14,21 @@ class LogJobProcessedListener
     {
         $payload = $event->job->payload();
 
-        $startTime = $payload['pushedAt'];
+        $startTime = $payload['pushedAt'] ?? null;
+
+        if(empty($startTime)) {
+            Log::info('Job Processed', [
+                'event' => 'job.processed',
+                'job' => $payload['displayName'] ?? 'Unknown Job',
+                'connection' => $event->connectionName,
+                'queue' => $event->job->getQueue(),
+                'payload' => $payload,
+                'processing_time' => null,
+            ]);
+
+            return;
+        }
+
         $endTime = microtime(true);
 
         $processingTime = ($endTime - $startTime) / 1000;
